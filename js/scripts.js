c03ba89a475b5b82bebe9e065b4c50400aa769f4
Łukasz Ustach <31926165+lukasszzo@users.noreply.github.com>
2018-01-17 18:47:58 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,79 +1,64 @@
 (function() {
 	document.addEventListener('DOMContentLoaded', function() {
-		var slider = document.querySelector('#main_slider');
-		var left = slider.querySelector('.left');
-		var right = slider.querySelector('.right');
-		var slides = slider.querySelectorAll('.slide');
-		var presentSlide = 0;
+		var slider = document.querySelector('#main-slider');
+		var prev = slider.querySelector(':scope .slider-prev');
+		var next = slider.querySelector(':scope .slider-next');
+		var slides = slider.querySelectorAll(':scope .slide');
+		var currentSlide = 0;
 		var timer = null;
 		var timeDelay = 5000;
 
-		var timeoutRightSlide = function() {
+		var timeoutNextSlide = function() {
 			timer = setTimeout(function() {
-				right.click();
+				next.click();
 			}, timeDelay);
 		};
 
-		var rightSlide = function() {
-
+		var prevSlide = function() {
 			for (var i=0; i<slides.length; i++) {
 				slides[i].classList.remove('active');
 			}
-			presentSlide--;
+			currentSlide--;
 
-
-			if (presentSlide < 0) {
-				presentSlide = slides.length-1;
+			if (currentSlide < 0) {
+				currentSlide = slides.length-1;
 			}
 
-
-			slides[presentSlide].classList.add('active');
-
-
+			slides[currentSlide].classList.add('active');
 			clearTimeout(timer);
-			timeoutRightSlide();
+			timeoutNextSlide();
 		};
 
-		var leftSlide = function() {
-
-
+		var nextSlide = function() {
 			for (var i=0; i<slides.length; i++) {
 				slides[i].classList.remove('active');
 			}
-			presentSlide++;
-
-			if (presentSlide > slides.length-1) {
-				presentSlide = 0;
+			currentSlide++;
+			if (currentSlide > slides.length-1) {
+				currentSlide = 0;
 			}
-			slides[presentSlide].classList.add('active');
-
+			slides[currentSlide].classList.add('active');
 			clearTimeout(timer);
-			timeoutRightSlide();
+			timeoutNextSlide();
 		};
+		timeoutNextSlide();
 
-
-		timeoutRightSlide();
-
-		left.addEventListener('click', leftSlide);
-		right.addEventListener('click', rightSlide);
+		prev.addEventListener('click', prevSlide);
+		next.addEventListener('click', nextSlide);
 
 	});
 
 })();
-
-
 (function() {
 	document.addEventListener('DOMContentLoaded', function() {
-		var boxes = document.querySelectorAll('.picture_center');
-		console.log(boxes)
-		var markBox = function(a) {
-			a.preventDefault();
+		var boxes = document.querySelectorAll('.picture-cnt');
+		var markBox = function(e) {
+			e.preventDefault();
 
 			for (var i=0; i<boxes.length; i++) {
 				boxes[i].classList.remove('active');
 			}
-
-			a.currentTarget.classList.add('active');
+			e.currentTarget.classList.add('active');
 		};
 
 		for (var i=0; i<boxes.length; i++) {
